56a60b24439d5ca3872d22ca9edf10be2b968199
jamez2128 <jamez2128@ArchPredator.localdomain>
2022-09-11 02:15:02 +0800

    Separted the Login class into a seperate file and added a new logo

--- src/_6_Week_6_Final_Project/GUI.java
+++ src/_6_Week_6_Final_Project/GUI.java
@@ -4,60 +4,12 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.function.Consumer;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
-
-class Login {
-	String phoneNumber;
-	String pinCode;
-	JLabel loginStatusLabel;
-	
-	JPanel loginPanel(Consumer<Integer> action) {
-		JPanel loginPanel = new JPanel();
-		loginPanel.setLayout(null);
-		
-		JLabel phoneNumberLabel = new JLabel("Phone Number:");
-		phoneNumberLabel.setBounds(10, 20, 100, 25);
-		
-		JTextField phoneNumberField = new JTextField(20);
-		phoneNumberField.setBounds(125, 20, 165, 25);
-		
-		JLabel pinCodeLabel = new JLabel("PIN Code:");
-		pinCodeLabel.setBounds(10, 50, 80, 25);
-		
-		JPasswordField pinCodeField = new JPasswordField(20);
-		pinCodeField.setBounds(125, 50, 165, 25);
-
-		loginStatusLabel = new JLabel("");
-		loginStatusLabel.setBounds(10, 110, 300, 25);
-
-		JButton loginButton = new JButton("Log in");
-		loginButton.setBounds(10, 80, 80, 25);
-		loginButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				phoneNumber = phoneNumberField.getText();
-				pinCode = pinCodeField.getText();
-				action.accept(null);
-				loginPanel.revalidate();
-				loginPanel.updateUI();
-			}
-		});
-
-		loginPanel.add(phoneNumberField);
-		loginPanel.add(phoneNumberLabel);
-		loginPanel.add(pinCodeLabel);
-		loginPanel.add(pinCodeField);
-		loginPanel.add(loginStatusLabel);
-		loginPanel.add(loginButton);
-		loginPanel.updateUI();
-		return loginPanel;
-	}
-}
 
 public class GUI {
 	JFrame frame;
@@ -112,6 +64,8 @@ public class GUI {
 		frame.setVisible(true);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ImageIcon logo = new ImageIcon("assets/images/logo.png");
+		frame.setIconImage(logo.getImage());
 		
 		mainMenuPanel = mainMenuPanel();
 		
