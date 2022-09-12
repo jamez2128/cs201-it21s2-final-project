973a507ad2f8d73a32bc9944dfe34ff9c2204ad3
jamez2128 <jamez2128@ArchPredator.localdomain>
2022-09-13 01:04:10 +0800

    Removed Login class

--- src/_6_Week_6_Final_Project/GUI.java
+++ src/_6_Week_6_Final_Project/GUI.java
@@ -1,24 +1,26 @@
 package _6_Week_6_Final_Project;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.function.Consumer;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class GUI {
 	JFrame frame;
 	JPanel mainMenuPanel;
-	Login login;
 	
 	JPanel mainMenuPanel() {
 		JPanel mainMenuPanel = new JPanel();
 		
 		JPanel servicesSubPanel = new JPanel();
+		servicesSubPanel.setPreferredSize(new Dimension(360, 500));
 		JPanel balanceSubPanel = new JPanel();
 		JLabel balanceLabel = new JLabel("Balance: P0.00");
 		JButton cashInButton = new JButton("Cash In");
@@ -75,23 +77,54 @@ public class GUI {
 	}
 	
 	JPanel newLogin() {
-		login = new Login();
-		JPanel loginPanel = login.loginPanel(new Consumer<Integer>() {
+		JPanel loginPanel = new JPanel();
+		loginPanel.setLayout(null);
+		
+		JLabel phoneNumberLabel = new JLabel("Phone Number:");
+		phoneNumberLabel.setBounds(10, 20, 100, 25);
+		
+		JTextField phoneNumberField = new JTextField(20);
+		phoneNumberField.setBounds(125, 20, 165, 25);
+		
+		JLabel pinCodeLabel = new JLabel("PIN Code:");
+		pinCodeLabel.setBounds(10, 50, 80, 25);
+		
+		JPasswordField pinCodeField = new JPasswordField(20);
+		pinCodeField.setBounds(125, 50, 165, 25);
+
+		JLabel loginStatusLabel = new JLabel("");
+		loginStatusLabel.setBounds(10, 110, 300, 25);
+
+		JButton loginButton = new JButton("Log in");
+		loginButton.setBounds(10, 80, 80, 25);
+		loginButton.addActionListener(new ActionListener() {
 			@Override
-			public void accept(Integer t) {
-				if (login.phoneNumber.equals("09454748745") && login.pinCode.equals("6969")) {
-					login.loginStatusLabel.setText("");
+			public void actionPerformed(ActionEvent e) {
+				String phoneNumber = phoneNumberField.getText();
+				String pinCode = pinCodeField.getText();
+				if (phoneNumber.equals("09454748745") && pinCode.equals("6969")) {
+					loginStatusLabel.setText("");
 					frame.getContentPane().removeAll();
 					mainMenuPanel = mainMenuPanel();
 					frame.add(mainMenuPanel);
 					frame.revalidate();
 					frame.repaint();
 				} else {
-					login.loginStatusLabel.setText("Credentials are not correct");
-					login.loginStatusLabel.setForeground(Color.red);
+					loginStatusLabel.setText("Credentials are not correct");
+					loginStatusLabel.setForeground(Color.red);
 				}
+				loginPanel.revalidate();
+				loginPanel.updateUI();
 			}
 		});
+
+		loginPanel.add(phoneNumberField);
+		loginPanel.add(phoneNumberLabel);
+		loginPanel.add(pinCodeLabel);
+		loginPanel.add(pinCodeField);
+		loginPanel.add(loginStatusLabel);
+		loginPanel.add(loginButton);
+		loginPanel.updateUI();
 		return loginPanel;
 	}
 	
@@ -101,7 +134,7 @@ public class GUI {
 		frame.setTitle("DigiCash");
 		frame.setSize(480,720);
 		frame.setVisible(true);
-		frame.setResizable(false);
+//		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ImageIcon logo = new ImageIcon("assets/images/logo.png");
 		frame.setIconImage(logo.getImage());
