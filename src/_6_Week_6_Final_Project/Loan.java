800affec24e114bb569b2171c2c866161b8c9726
turiano <juricuber@gmail.com>
2022-09-16 17:36:35 +0800

    New loan gui

--- src/_6_Week_6_Final_Project/Loan.java
+++ src/_6_Week_6_Final_Project/Loan.java
@@ -1,15 +1,19 @@
 package _6_Week_6_Final_Project;
 
-import javax.swing.JOptionPane;
-public class Loan {
+import java.awt.Dimension;
 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+public class Loan {
+	
 	public static void Loans() {
-		
-		int num;
-		
-		num = Integer.parseInt(JOptionPane.showInputDialog(null, "How much Loan do you want?", "Loan", JOptionPane.PLAIN_MESSAGE));
-		
-		JOptionPane.showMessageDialog(null, "Your Loan that you borrow: " + num, "Loan", JOptionPane.PLAIN_MESSAGE);
+		JFrame frame = new JFrame();
+		ImageIcon logo = new ImageIcon("assets/images/logo.png");	
+		frame.setSize(400, 300);
+		frame.setName("Loan");
+		frame.setVisible(true);
+		frame.setResizable(false);
 	}
 
 }
