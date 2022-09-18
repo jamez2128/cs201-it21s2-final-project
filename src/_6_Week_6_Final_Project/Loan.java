c95334e9de083350351fec5b4ca0bd71590469c3
turiano <juricuber@gmail.com>
2022-09-18 23:29:31 +0800

    finished loan ui

--- src/_6_Week_6_Final_Project/Loan.java
+++ src/_6_Week_6_Final_Project/Loan.java
@@ -1,9 +1,11 @@
 package _6_Week_6_Final_Project;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,28 +15,28 @@ public class Loan {
 	
 	static JPanel Loans() {
 		JPanel panel = new JPanel();
-			panel.setSize(400, 200);
+			panel.setSize(300, 200);
+			panel.setLayout(null);
 			
 		JLabel label = new JLabel("Please enter the value: ");
-			label.setFont(new Font(label.getFont().getName(), 15, 15));
+			label.setBounds(80,20,150,30);
+			label.setFont(new Font(label.getFont().getName(), 18, 18));
 			label.setFont(new Font(label.getFont().getName(), Font.BOLD, 13));
 			
 		JTextField textbox = new JTextField(20);
-			label.setBounds(20, 40, 0, 0);
+			textbox.setBounds(60, 50, 182, 25);
 		
 		JButton enter = new JButton("Enter");
-			enter.setBounds(25, 20, 0, 0);
-			
+			enter.setBounds(60, 80, 80, 25);
+		
 		JButton cancel = new JButton("Cancel");
-			cancel.setBounds(25, 20, 0, 0);
-		
-		
-			
+			cancel.setBounds(161, 80, 80, 25);		
+
 			panel.add(label);
 			panel.add(textbox);
 			panel.add(enter);
 			panel.add(cancel);
-
+			
 		return panel;
 	}
 	
@@ -47,12 +49,13 @@ public class Loan {
 	
 	public static void mFrame() {
 		JFrame frame = new JFrame();	
-		frame.setSize(400, 200);
+		frame.setSize(300, 200);
 		frame.setTitle("Loan");
 		frame.setVisible(true);
 		frame.setResizable(false);
 		frame.add(Loans());
-		frame.setLayout(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 	}
 
 }
